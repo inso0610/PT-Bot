2402e3cc6903ec67835bbd84390d1c9e0380068e
Emil Velken Soldal <emil.v.soldal@gmail.com>
2023-09-17 13:52:24 +0200

    Added cool stuff

--- src/events/messageCreate/annanas-pizza.js
+++ src/events/messageCreate/annanas-pizza.js
@@ -1,14 +1,19 @@
-module.exports = (message) => {
+module.exports = async (message) => {
     if (message.author.bot) {
         return;
     }
+
+    let pineapple
 
     if (message.content.includes('pineapple')) {
 
         if (message.content.includes('pizza')) {
 
-            message.reply('Pineapple on pizza is the best!')
+            pineapple = await message.reply('Pineapple on pizza is the best!')
             // message.reply('People have their own meanings so I wont have a meaning in this. - <@764095520440188978>, 16.09.2023 21:46')
+
+            pineapple.react('🍍')
+            pineapple.react('🍕')
 
         } else{
 
