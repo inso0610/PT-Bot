8fa8e994764bc7bcbd036b29aae76055b791e8f0
Emil Velken Soldal <emil.v.soldal@gmail.com>
2023-09-17 14:04:24 +0200

    Ungdomsrådet

---
+++ src/events/messageCreate/ungdomsrådet.js
@@ -0,0 +1,22 @@
+module.exports = async (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    const text = message.content
+    const result = text.toLowerCase();
+    let ungdomsrådet
+
+    if (result.includes('ungdomsrådet')) {
+
+        ungdomsrådet = await message.reply('Ungdomsrådet is the worst! :face_womiting:')
+        // message.reply('People have their own meanings so I wont have a meaning in this. - <@764095520440188978>, 16.09.2023 21:46')
+
+        ungdomsrådet.react('🤮')
+
+    } else{
+
+        return;
+
+    } 
+};
