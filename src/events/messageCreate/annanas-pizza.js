6352010ac4994aaec034a6618d5fa48795de640c
Emil Velken Soldal <emil.v.soldal@gmail.com>
2023-09-17 20:06:16 +0200

    Stuff

--- src/events/messageCreate/annanas-pizza.js
+++ src/events/messageCreate/annanas-pizza.js
@@ -7,20 +7,22 @@ module.exports = async (message) => {
     const result = text.toLowerCase();
     let pineapple
 
-    if (result.includes('pineapple')) {
-
-        if (result.includes('pizza')) {
-
-            pineapple = await message.reply('Pineapple on pizza is the best!')
-            // message.reply('People have their own meanings so I wont have a meaning in this. - <@764095520440188978>, 16.09.2023 21:46')
-
-            pineapple.react('🍍')
-            pineapple.react('🍕')
-
-        } else{
-
-            return;
-
+    if (!result === 'pineapple on pizza sucks if you agree type ‘’Pineapple on pizza is the best!’’') {
+        if (result.includes('pineapple')) {
+    
+            if (result.includes('pizza')) {
+    
+                pineapple = await message.reply('Pineapple on pizza is the best!')
+                // message.reply('People have their own meanings so I wont have a meaning in this. - <@764095520440188978>, 16.09.2023 21:46')
+    
+                pineapple.react('🍍')
+                pineapple.react('🍕')
+    
+            } else{
+    
+                return;
+    
+            }
         }
     } 
 };
