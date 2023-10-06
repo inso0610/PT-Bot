3f83b53d7914a3442ad67f50f8bd8744d2ab3821
Emil Velken Soldal <emil.v.soldal@gmail.com>
2023-10-06 21:51:00 +0200

    DM Application feedback hopefully completed

--- src/commands/application-stuff/dm-application-results.js
+++ src/commands/application-stuff/dm-application-results.js
@@ -10,7 +10,7 @@ module.exports = {
             .setTitle('Application Feedback')
             .setDescription(`# Driver Manager application results:
 
-            :white_check_mark: :clap: | Congratulations you passed the Driver Manager application!
+            ✅ 👏 | Congratulations you passed the Driver Manager application!
                 
             ## Feedback:
             -`);
@@ -20,7 +20,7 @@ module.exports = {
                     .setTitle('Application Feedback')
                     .setDescription(`# Driver Manager application results:
 
-                    :white_check_mark: :clap: | Congratulations you passed the Driver Manager application!
+                    ✅ 👏 | Congratulations you passed the Driver Manager application!
                 
                     ## Feedback:
                     Your application looks really good, but I want you to improve on these points during your training period:
@@ -44,6 +44,123 @@ module.exports = {
                     console.log(error);
                 }
 
+            } else if (interaction.user.id === '1102514225181630476') {
+
+                feedbackEmbed = new EmbedBuilder()
+                    .setTitle('Application Feedback')
+                    .setDescription(`# Driver Manager application results:
+
+                    ❌ | Congratulations you passed the Driver Manager application!
+                
+                    ## Feedback:
+                    Mostly your application lacked in reasoning and explenation.
+                    Something that I think is great is how well your knowledge on our signal system is. 
+                    Had you elaborated more on your application you would have passed.`);
+
+                try {
+                    interaction.user.send({
+                        embeds: [ feedbackEmbed ]
+                    });
+                interaction.reply({
+                    content: 'Your feedback has been sent to your DM\'s.',
+                    ephemeral: true
+                });
+                } catch (error) {
+                    interaction.reply({
+                        content: 'I was not able to send you a DM. Maybe try checking if you allow DM\'s from this server.',
+                        ephemeral: true
+                    });
+                
+                    console.log(error);
+                }
+            
+            } else if (interaction.user.id === '1004003667940225105') {
+                
+                feedbackEmbed = new EmbedBuilder()
+                    .setTitle('Application Feedback')
+                    .setDescription(`# Driver Manager application results:
+
+                    ✅ 👏 | Congratulations you passed the Driver Manager application!
+                
+                    ## Feedback:
+                    Your application looks good and I can see that you are engaged in Polar Tracks.
+                    I would also like to remind you that as a Driver Manager you are a mod and you should also concider demoting people when not following the driving rules.`);
+
+                try {
+                    interaction.user.send({
+                        embeds: [ feedbackEmbed ]
+                    });
+                interaction.reply({
+                    content: 'Your feedback has been sent to your DM\'s.',
+                    ephemeral: true
+                });
+                } catch (error) {
+                    interaction.reply({
+                        content: 'I was not able to send you a DM. Maybe try checking if you allow DM\'s from this server.',
+                        ephemeral: true
+                    });
+                
+                    console.log(error);
+                }
+
+            } else if (interaction.user.id === '873555071747637258') {
+
+                feedbackEmbed = new EmbedBuilder()
+                    .setTitle('Application Feedback')
+                    .setDescription(`# Driver Manager application results:
+
+                    ❌ | Congratulations you passed the Driver Manager application!
+                
+                    ## Feedback:
+                    Mostly your application lacked in reasoning and explenation.
+                    You should also read up on our signals in our driver guide (https://drive.google.com/file/d/1qiDRksoauycsIStmTyMYYmXAQAmO2zCq/view?usp=sharing)`);
+
+                try {
+                    interaction.user.send({
+                        embeds: [ feedbackEmbed ]
+                    });
+                interaction.reply({
+                    content: 'Your feedback has been sent to your DM\'s.',
+                    ephemeral: true
+                });
+                } catch (error) {
+                    interaction.reply({
+                        content: 'I was not able to send you a DM. Maybe try checking if you allow DM\'s from this server.',
+                        ephemeral: true
+                    });
+                
+                    console.log(error);
+                }
+            
+            } else if (interaction.user.id === '859003676114812939') {
+
+                feedbackEmbed = new EmbedBuilder()
+                    .setTitle('Application Feedback')
+                    .setDescription(`# Driver Manager application results:
+
+                    ❌ | Congratulations you passed the Driver Manager application!
+                
+                    ## Feedback:
+                    Mostly your application lacked in reasoning and explenation.
+                    You should also read up on our signals in our driver guide (https://drive.google.com/file/d/1qiDRksoauycsIStmTyMYYmXAQAmO2zCq/view?usp=sharing)`);
+
+                try {
+                    interaction.user.send({
+                        embeds: [ feedbackEmbed ]
+                    });
+                interaction.reply({
+                    content: 'Your feedback has been sent to your DM\'s.',
+                    ephemeral: true
+                });
+                } catch (error) {
+                    interaction.reply({
+                        content: 'I was not able to send you a DM. Maybe try checking if you allow DM\'s from this server.',
+                        ephemeral: true
+                    });
+                
+                    console.log(error);
+                }
+            
             } else {
                 interaction.reply({
                     content: `We do not have any feedback ready for you. This could be because we have not finished writing it or you have not applied for driver manager.`,
@@ -57,6 +174,6 @@ module.exports = {
         guildOnly: false,
         userPermissions: [],
         botPermissions: ['Administrator'],
-        deleted: true,
+        deleted: false,
     },
 };
