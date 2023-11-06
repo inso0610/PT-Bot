be4d1861173e5640291e0a1a1a5b968a04b9a0d5
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2023-11-06 14:23:44 +0100

    Fixed stuff

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -3,10 +3,17 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('help')
-    .setDescription('Sends a list of all commmands and their function.'),
+    .setDescription('Sends a list of all commmands and their function.')
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('commands')
+            .setDescription('Sends a list of all commmands and their function.')),
 
     run: async ({ interaction, client, handler }) => {
-        const helpEmbed = new EmbedBuilder()
+        const subcommand = interaction.options.getSubcommand();
+
+        if (subcommand === "commands") {
+            const helpEmbed = new EmbedBuilder()
             .setTitle('Commands:')
             .addFields(
                 { name: '/help:', value: 'Sends a list of all commmands and their function.' },
@@ -17,7 +24,13 @@ module.exports = {
             interaction.reply({
                 embeds: [ helpEmbed ],
                 ephemeral: true
+            });
+        } else {
+            interaction.reply({
+                content: 'Command failure. Try again.',
+                ephemeral: true
             })
+        }
     },
 
     options: {
