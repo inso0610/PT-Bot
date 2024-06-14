7dbbc758df4236831796a5df7b96714cff3dc91c
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2024-06-14 11:34:37 +0200

    Removed unnecessary stuff

--- src/events/ready/activity.js
+++ src/events/ready/activity.js
@@ -20,6 +20,8 @@ const status = [
 ];
 
 module.exports = (client) => {
+    console.log(`${client.user.tag} is online.`)
+
     setInterval(() => {
         let random = Math.floor(Math.random() * status.length);
         client.user.setActivity(status[random]);
