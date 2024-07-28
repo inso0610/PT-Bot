16b8abdf75b0686a59b04eee9c69531de79a8b69
Emil Velken Soldal <emil.v.soldal@gmail.com>
2024-07-28 12:05:26 +0200

    Added Platform Manager feedback and removed old Signaller Manager feedback.

--- src/commands/application-stuff/dm-application-results.js
+++ src/commands/application-stuff/dm-application-results.js
@@ -25,29 +25,17 @@ module.exports = {
         */
 
         const feedback = {
-            '797752888347852850':`# Signaller Manager application results:
-            ✅ 👏 | Congratulations you passed the Signaller Manager application!
+            '1025727327461703720':`# Platform Manager application results:
+            ✅ 👏 | Congratulations you passed the Platform Manager application!
                             
             ## Feedback:
-            You answers were all short and did not explain to us why we should pick you. Even though Caiber wanted you to apply answering "Because cabier wants me" is not good enough. Your signalling knowledge is good and you passed that part however your answers in the general questions part were not good enough.`,
+            Your answers were short, but you showd that you had a good amount of knowledge on the role of Dispatcher. The only big error you did was that you did not know when proceed should be used and when shunt with caution should be used.`,
 
-            '796031038203494420':`# Signaller Manager application results:
-            ❌ | I'm sorry, but you have failed the Signaller Manager application.
+            '606391401889923073':`# Platform Manager application results:
+            ❌ | I'm sorry, but you have failed the Platform Manager application.
                             
             ## Feedback:
-            Your answers were short in the general questions part. You knew most of the signalling theory, but yellow means that the signal is set to diverging not that the switches are getting set like you wrote.`,
-
-            '1102514225181630476':`# Signaller Manager application results:
-            ❌ | I'm sorry, but you have failed the Signaller Manager application.
-                            
-            ## Feedback:
-            Your answers in the general questions part were good and you knew the signalling theory. However there were better candidates. If you apply when the next application open's you will probably pass.`,
-            
-            '1004003667940225105':`# Signaller Manager application results:
-            ✅ 👏 | Congratulations you passed the Signaller Manager application!
-                            
-            ## Feedback:
-            Your answers in the general questions part were good and your signalling theory answers were really good.`
+            Your answers were short and you were unable to answer most questions.`
         };
 
         const omChat = client.channels.cache.get('1159950672322633809');
