63b6291eb3239c0c47cdc459c2916f415302ab0b
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2024-09-17 12:08:32 +0200

    Changed log

--- src/events/messageCreate/counting.js
+++ src/events/messageCreate/counting.js
@@ -28,7 +28,7 @@ module.exports = async (message) => {
 
     const nextNumber = await counting.findById('66e9500b12c20d26f47cdd88').exec();
 
-    console.log(nextNumber.nextNumber)
+    console.log(nextNumber)
 
     if (numberInText === nextNumber.nextNumber && message.author.id !== nextNumber.lastNumberSenderId) {
         nextNumber.nextNumber = numberInText + 1;
