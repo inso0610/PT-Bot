0cf2db141cc95e9a950b07ea5468b1d842236f6d
Emil Velken Soldal <emil.v.soldal@gmail.com>
2024-11-01 16:57:24 +0100

    Ping event thingy

---
+++ src/events/messageCreate/ping.js
@@ -0,0 +1,18 @@
+module.exports = (message) => {
+    if (message.author.bot) {
+        return;
+    };
+
+    const channelId = message.channelId;
+    const content = message.content;
+
+    if (content.includes('[PING]') && interaction.member.roles.cache.has('1111370796439453777')) {
+        if (channelId === '1246904420495523925') { //trainings
+            message.reply('^<@&1140220447535923200>');
+        } else if (channelId === '1149803344852959273'){ //event-announcements
+            message.reply('^<@&1149802530507862056>');
+        } else if (channelId === '1140363643347816458'){ //shift-announcements
+            message.reply('^<@&1140248514568405044>');
+        };
+    };
+};
