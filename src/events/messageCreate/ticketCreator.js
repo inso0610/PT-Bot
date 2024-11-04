c8edc8758b4fee5581b7093b19030a8911e43101
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2024-11-04 13:09:22 +0100

    a

--- src/events/messageCreate/ticketCreator.js
+++ src/events/messageCreate/ticketCreator.js
@@ -2,12 +2,15 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = async (message, client) => {
     if (message.author.bot || message.guild !== null) {
+        console.log('1')
         return;
     };
+    console.log('2')
 
     console.log(message.mentions.first())
 
     if (message.mentions.has(client.user)) {
+        console.log('3')
         const WelcomeEmbed = new EmbedBuilder()
             .setTitle('Welcome to our ticket system!')
             .setDescription('Please reply with the topic of your ticket.');
