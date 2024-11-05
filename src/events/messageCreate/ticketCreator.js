24d107ee047981fc29df8c915a0ef0b0b45efb7b
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2024-11-05 09:56:20 +0100

    Fix

--- src/events/messageCreate/ticketCreator.js
+++ src/events/messageCreate/ticketCreator.js
@@ -16,6 +16,8 @@ module.exports = async (message, client) => {
             embeds: [WelcomeEmbed]
         });
 
+        const messageFilter = (m) => m.author === message.author;
+
         const response1 = await message.channel.awaitMessages(filter, {max: 1, time: 60_000});
 
         if (!response1) {
