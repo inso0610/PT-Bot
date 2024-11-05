355f774d1aee002119f5b8f9c12e64bcea48d1c4
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2024-11-05 09:29:06 +0100

    Small add

--- src/events/messageCreate/ticketCreator.js
+++ src/events/messageCreate/ticketCreator.js
@@ -3,18 +3,26 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = async (message, client) => {
     console.log('TicketCreator Event')
 
-    if (message.author.bot || message.guild !== null) {
+    if (message.author.bot || message.guild !== null || message.content) {
         return;
     };
 
     if (message.mentions.has(client.user)) {
-        console.log('3')
         const WelcomeEmbed = new EmbedBuilder()
             .setTitle('Welcome to our ticket system!')
             .setDescription('Please reply with the topic of your ticket.');
 
-        client.users.send(message.author.id, {
+        await message.reply({
             embeds: [WelcomeEmbed]
         });
+
+        const response1 = await message.channel.awaitMessages(filter, {max: 1, time: 60_000});
+
+        if (!response1) {
+            client.users.send(message.user.id, 'The ticket creator timed out. You can ping me if you need more help.');
+            return;
+        };
+
+        console.log(response1.content);
     };
 };
