958940a900e82d073a9038ad86d1b036e5469647
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2025-01-16 14:00:44 +0100

    Started minor work on get trainings

---
+++ src/commands/trainingStuff/get-trainings.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const trainings = require('../../utils/trainings.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('get-trainings')
+    .setDescription('Get your trainings'),
+    run: async ({ interaction, client, handler }) => {
+
+        interaction.reply({
+            content: 'Please check your dms.',
+            ephemeral: true
+        });
+
+        const assignedTrainings = trainings.find(hostDiscordId = interaction.user.id)
+
+
+    },
+    opTeamOnly: true,
+
+    options: {
+        devOnly: false,
+        userPermissions: [],
+        botPermissions: ['Administrator'],
+        deleted: true,
+    },
+};
