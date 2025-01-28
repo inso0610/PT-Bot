ed45d77e04ba2fc3c2aa96d3c0afd03230987ce8
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-01-28 20:58:59 +0100

    Added message command

---
+++ src/commands/misc/message.js
@@ -0,0 +1,106 @@
+const { SlashCommandBuilder, } = require('discord.js');
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('message')
+    .setDescription('Sends a message to someone using the PT Bot')
+    .addUserOption((option) =>
+        option
+            .setName('reciever')
+            .setDescription('Who should recieve this message?')
+            .setRequired(true))
+    .addStringOption((option) => 
+        option
+            .setName('sender-team')
+            .setDescription('What team is sending the message?')
+            .setRequired(true)
+            .addChoices(
+                { name: 'Senior Management', value: 'Senior Management' },
+                { name: 'Directors', value: 'Director' },
+                { name: 'Group Advisors', value: 'Group Advisor' }
+            ))
+    .addStringOption((option) => 
+        option
+            .setName('text')
+            .setDescription('What text should the person recieve?')
+            .setRequired(true))
+    .addBooleanOption((option) => 
+        option
+            .setName('show-sender')
+            .setDescription('Do you want the recieved to know who sent the message?')
+            .setRequired(true)),
+
+    run: async ({ interaction, client, handler }) => {
+        const reciever = interaction.options.getUser('reciever');
+        const team = interaction.options.getString('sender-team');
+        const text = interaction.options.getString('text');
+        const showSender = interaction.options.getBoolean('show-sender');
+
+        if (team === 'Directors' && !interaction.member.roles.cache.has('1140260309915938866')) {
+            interaction.reply({
+                content: "You can't send a message as a Director.",
+                ephemeral: true
+            });
+            return;
+        } else if (team === 'Group Advisors' && !interaction.member.roles.cache.has('1089284396282032178')) {
+            interaction.reply({
+                content: "You can't send a message as a Group Advisor.",
+                ephemeral: true
+            });
+            return;
+        };
+
+        if (showSender) {
+            const teamRoles = {
+                ['Senior Management']: 'Senior Manager',
+                Director: 'Director',
+                ['Group Advisor']: 'Group Advisor',
+            }
+
+            const role = teamRoles[team];
+
+            const message = await reciever.send(`Hello! You have recieved a message from ${role} ${interaction.member.nickname}.\n\`\`\`${text}\`\`\`\nIf you want to respond to this message please create a ticket by pinging me.`).catch(e => {
+                console.warn(e);
+                interaction.reply({
+                    content: 'I could not message the user.',
+                    ephemeral: true
+                });
+                return false;
+            });
+
+            if (message !== false) {
+                interaction.reply({
+                    content: 'Sent the message.',
+                    ephemeral: true
+                });
+            }
+        } else {
+            const message = await reciever.send(`Hello! You have recieved a message from the ${team} team.\n\`\`\`${text}\`\`\`\nIf you want to respond to this message please create a ticket by pinging me.`).catch(e => {
+                console.warn(e);
+                interaction.reply({
+                    content: 'I could not message the user.',
+                    ephemeral: true
+                });
+                return false;
+            });
+
+            if (message !== false) {
+                interaction.reply({
+                    content: 'Sent the message.',
+                    ephemeral: true
+                });
+            }
+        };
+    },
+    smOnly: true,
+
+    options: {
+        devOnly: false,
+        guildOnly: false,
+        userPermissions: [],
+        botPermissions: ['Administrator'],
+        deleted: false,
+    },
+};
