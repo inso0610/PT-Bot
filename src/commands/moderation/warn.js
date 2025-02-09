73a69d1e63aaa58b8dd47f46bda975e50eab067e
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-09 14:36:24 +0100

    Add 'warn' command for user moderation with permission settings

---
+++ src/commands/moderation/warn.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const warnings = require('../../utils/moderation/warnings');
+const modlogs = require('../../utils/moderation/modlogs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('warn')
+    .setDescription('Warn a user.')
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+    run: async ({ interaction, client, handler }) => {
+        
+    },
+    modOnly: true,
+
+    options: {
+        devOnly: false,
+        guildOnly: false,
+        userPermissions: [],
+        botPermissions: ['Administrator'],
+        deleted: true,
+    },
+}
