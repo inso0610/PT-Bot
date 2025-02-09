ab4f6370c0c36312fe0fe30783438222652c207b
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-09 18:23:15 +0100

    Add message logging for deleted and updated messages with webhook integration

---
+++ src/events/messageDelete/message-logger-delete.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const { EmbedBuilder, WebhookClient } = require("discord.js");
+
+module.exports = (message) => {
+    const webhookClient = new WebhookClient({ id: '1338191419709591682', token: '_bGgIHOt6m5VFFqhahwKNj8b_9M8fkbMueVEfRWz9fY2GEwAMybKYjuKcEsXkEoJ_YJ0' }); 
+    
+    const ignoredCategories = ['1145464301965037588', '1133750227002736732', '1294325748043092071', 
+        '1141398695951208559', '1096576167731023932', '1096576509063471124', '1096575749462425791',
+        '1096575490078285967', '1304867108668309604']
+    const ignoredChannels = []
+
+    try {
+        if (message.author.bot) {
+            return;
+        } else if (ignoredCategories.includes(message.channel.parent.id)) {
+            return;
+        } else if (ignoredChannels.includes(message.channel.id)) {
+            return;
+        } else if (message.webhookId){
+            return;
+        } else{
+
+            const deletedMessage = new EmbedBuilder()
+            .setTitle(`Deleted message from: ${message.author.username}`)
+            .setDescription(message.content)
+            .addFields(
+                { name: 'Channel:', value: `<#${message.channel.id}>` }
+            );
+
+            webhookClient.send({
+                username: (`${message.author.username} (${message.author.id})`),
+                avatarURL: message.author.avatarURL(),
+                embeds: [ deletedMessage ]
+            }).catch(e => {
+                console.warn(`Error in message-logger-delete: ${e}`)
+                return;
+            });
+        }
+    } catch (error) {
+        console.warn(`Error in message-logger-delete: ${error}`)
+    }
+}
+
+//https://discord.com/api/webhooks/1338191419709591682/_bGgIHOt6m5VFFqhahwKNj8b_9M8fkbMueVEfRWz9fY2GEwAMybKYjuKcEsXkEoJ_YJ0
---
+++ src/events/messageUpdate/message-logger-update.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { EmbedBuilder, WebhookClient } = require("discord.js");
+
+module.exports = (oldMessage, newMessage) => {
+    const webhookClient = new WebhookClient({ id: '1338191419709591682', token: '_bGgIHOt6m5VFFqhahwKNj8b_9M8fkbMueVEfRWz9fY2GEwAMybKYjuKcEsXkEoJ_YJ0' }); 
+    
+    const ignoredCategories = ['1145464301965037588', '1133750227002736732', '1294325748043092071', 
+        '1141398695951208559', '1096576167731023932', '1096576509063471124', '1096575749462425791',
+        '1096575490078285967', '1304867108668309604']
+    const ignoredChannels = []
+
+    try {
+        if (newMessage.author.bot) {
+            return;
+        } else if (ignoredCategories.includes(newMessage.channel.parent.id)) {
+            return;
+        } else if (ignoredChannels.includes(newMessage.channel.id)) {
+            return;
+        } else if (newMessage.webhookId){
+            return;
+        } else{
+
+            const info = new EmbedBuilder()
+                .setTitle(`${newMessage.author.username} Edited their message`)
+                .addFields(
+                    { name: 'Kanal:', value: `<#${newMessage.channel.id}>` }
+                );
+            const gammelMelding = new EmbedBuilder().setTitle('Old message:').setDescription(oldMessage.content);
+            const nyMelding = new EmbedBuilder().setTitle('New message:').setDescription(newMessage.content);
+
+            webhookClient.send({
+                username: (`${newMessage.author.username} (${newMessage.author.id})`),
+                avatarURL: newMessage.author.avatarURL(),
+                embeds: [info, gammelMelding, nyMelding]
+            }).catch(e => {
+                console.warn(`Error in message-logger-update: ${e}`)
+                return;
+            });
+        }
+    } catch (error) {
+        console.warn(`Error in message-logger-update: ${error}`)
+    }
+}
+
+//https://discord.com/api/webhooks/1338191419709591682/_bGgIHOt6m5VFFqhahwKNj8b_9M8fkbMueVEfRWz9fY2GEwAMybKYjuKcEsXkEoJ_YJ0
