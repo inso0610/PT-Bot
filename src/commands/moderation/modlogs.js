99c61124c000c7f29bfaaed1e0500b311696dfdd
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-09 16:12:02 +0100

    Add 'timeout' command for moderating users with optional warning logging

--- src/commands/moderation/modlogs.js
+++ src/commands/moderation/modlogs.js
@@ -21,7 +21,7 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle(`Moderation Logs for ${user.tag}`)
-            .setDescription(logs.map((log, index) => `**Log ${toString(log._id)}**\n**Action:** ${log.action}\n**Reason:** ${log.reason}\n**Moderator:** ${log.moderatorUsername}\n**Date:** ${log.doneAt}`).join('\n\n'));
+            .setDescription(logs.map((log, index) => `**Log ${log._id.toString()}**\n**Action:** ${log.action}\n**Reason:** ${log.reason}\n**Moderator:** ${log.moderatorUsername}\n**Date:** ${log.doneAt}`).join('\n\n'));
 
         interaction.reply({ embeds: [embed], ephemeral: true });
     },
---
+++ src/commands/moderation/timeout.js
@@ -0,0 +1,119 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const warnings = require('../../utils/moderation/warnings');
+const modlogs = require('../../utils/moderation/modlogs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('timeout')
+    .setDescription('Timeout someone in the Discord server')
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addUserOption((option) =>
+        option
+            .setName('user')
+            .setDescription('Who do you want to timeout?')
+            .setRequired(true))
+    .addNumberOption((option) =>
+        option
+            .setName('duration')
+            .setDescription('How long do you want to timeout this user? (in minutes)')
+            .setRequired(true))
+    .addStringOption((option) =>
+        option
+            .setName('reason')
+            .setDescription('Why are you timeouting this user?')
+            .setRequired(true))
+    .addBooleanOption((option) =>
+        option
+            .setName('warn')
+            .setDescription('Do you want to log a warning together with the timeout?')
+            .setRequired(false)),
+
+    run: async ({ interaction, client, handler }) => {
+        const commandUser = interaction.options.getUser('user');
+        const duration = interaction.options.getNumber('duration');
+        const reason = interaction.options.getString('reason');
+        const warn = interaction.options.getBoolean('warn') || false;
+
+        const moderator = interaction.guild.members.cache.get(interaction.user.id);
+        const user = interaction.guild.members.cache.get(commandUser.id);
+
+        if (!moderator || !user) {
+            return interaction.reply({ content: 'An error occurred while fetching user data.', ephemeral: true });
+        }
+
+        if (user.id === interaction.user.id) {
+            return interaction.reply({ content: 'You can\'t timeout yourself!', ephemeral: true });
+        }
+
+        if (user.id === client.user.id) {
+            return interaction.reply({ content: 'You can\'t timeout me!', ephemeral: true });
+        }
+
+        if (!user.kickable) {
+            return interaction.reply({ content: 'I can\'t timeout this user!', ephemeral: true });
+        }
+        
+        // Compare the highest roles
+        if (moderator.roles.highest.comparePositionTo(user.roles.highest) <= 0) {
+            return interaction.reply({ 
+                content: 'You can\'t timeout this user because they have a higher or equal role than you!', 
+                ephemeral: true 
+            });
+        }
+
+        // Timout the user
+        await user.timeout(duration * 60 * 1000, reason).catch(() => {
+            return interaction.reply({ content: 'I couldn\'t timeout this user!', ephemeral: true });
+        });
+
+        const message = await user.send(`You have been given a timeout in the Polar Tracks Discord server for the following reason: ${reason}.${warn ? '\n**⚠️ A warning was also applied.**' : ''}`).catch(() => {
+            return false;
+        });
+
+        // Log the action
+        const timeoutLog = new modlogs({
+            discordId: user.id,
+            action: 'timeout',
+            reason,
+            moderatorId: interaction.user.id,
+            moderatorUsername: interaction.user.username
+        });
+
+        timeoutLog.save();
+
+        // Warn the user
+
+        if (warn) {
+            const warning = new warnings({
+                discordId: user.id,
+                reason,
+                moderatorId: interaction.user.id,
+                moderatorUsername: interaction.user.username
+            });
+
+            warning.save();
+
+            const warnLog = new modlogs({
+                discordId: user.id,
+                action: 'warn',
+                reason: 'This warning was applied together with a timeout: ' + reason,
+                moderatorId: interaction.user.id,
+                moderatorUsername: interaction.user.username
+            });
+
+            warnLog.save();
+        };
+
+        return interaction.reply({ content: `User <@${user.id}> has been given a timeout. ${message ? '' : 'Failed to message this user.'}`, ephemeral: true });
+    },
+    modOnly: true,
+
+    options: {
+        devOnly: false,
+        guildOnly: false,
+        userPermissions: [],
+        botPermissions: ['Administrator'],
+        deleted: false,
+    },
+}
