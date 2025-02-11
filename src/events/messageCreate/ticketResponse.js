0de646117f9369132bbf689f53d450f92ec98045
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-11 17:12:18 +0100

    Refactor ticket response handling for improved clarity and error management

--- src/events/messageCreate/ticketResponse.js
+++ src/events/messageCreate/ticketResponse.js
@@ -1,144 +1,98 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 const { Types } = require('mongoose');
 const tickets = require('../../utils/tickets.js');
-
-const { ticketChannels } = require( '../../utils/ticketChannels.js');
+const { ticketChannels } = require('../../utils/ticketChannels.js');
 
 module.exports = async (message, client) => {
     try {
         const sendDM = async (messageContent) => {
-            //return message.author.dmChannel.send({ content: messageContent }).catch(e => {
-            return message.author.send(messageContent).catch(e => {    
+            return message.author.send(messageContent).catch(e => {
                 message.reply({
                     content: "I can't DM you, please check your DM settings!"
-                }).catch(e2 => {
-                    console.warn(e2);
-                });
+                }).catch(console.warn);
                 console.warn(e);
-    
-                return [e]
             });
         };
-    
-        if (message.author.bot) {
-            return;
-        };
-    
-        if (message.guild) {
-            return;
-        };
-    
+
+        if (message.author.bot || message.guild) return;
+
         const ticketIdString = message.content.match(/\[(.*?)\]/)?.[1];
-    
-        /*if (!mongoose.isValidObjectId(ticketIdString)) {
-            return;
-        };*/
-    
-        let ticketId;
-        if (Types.ObjectId.isValid(ticketIdString)) {
-            ticketId = new Types.ObjectId(ticketIdString);
-        } else {
-            return;
-        };
-    
-        if (!ticketId) {
-            return;
-        }
-    
-        const ticket = await tickets.findOne({creatorId: message.author.id, _id: ticketId}).exec()
-    
-        if (!ticket) {
-            return;
-        };
-    
-        const content = message.content.replace(/\[.*?\]/, '');
-
-        if (content.length > 500) {
-            sendDM('This response goes over our 500 character limit. Please shorten down your response or send it as multiple messages.')
-            return;
-        }
-    
-        if (ticket.claimedId === '0') {
-            sendDM('This ticket isn\'t claimed currently. Please wait to respond before it has been claimed.')
-            return;
-        };
-    
-        ticket.log.push(`<@${message.author.id}>: ${content}`);
-    
-        ticket.save();
-    
-        const claimedUser = await client.users.fetch(ticket.claimedId);
-    
-        if (!claimedUser) {
-            return interaction.editReply({
-                content: 'Could not find the user.',
-                ephemeral: true
-            });
-        };
-    
-        claimedUser.send(`Reply from <@${message.author.id}> for the ticket with the id \`${String(ticket._id)}\`:\n\`\`\`${content}\`\`\``).catch(e => {    
-            message.reply({
-                content: "Something went wrong when replying to the ticket. It will be set to unclaimed to avoid further issues."
-            }).catch(e2 => {
-                console.warn(e2);
-            });
-            console.warn(e);
-    
-            ticket.claimedUser = '0';
-    
-            ticket.save();
-    
-            const departmentSplit = ticket.department.split('-')
-    
-            const category = ticketChannels[departmentSplit[0]]
-    
-            const pings = category.pings;
-    
-            const channel = client.channels.cache.get(category.channel);
-    
-            const ticketMessage = channel.messages.fetch(ticket.ticketMessageId);
-    
-            const ticketEmbed = new EmbedBuilder()
-            .setTitle('🎫 Ticket')
-            .setDescription('Awaiting to be claimed')
-            .addFields(
-                {name: 'ID', value: String(ticket._id)},
-                {name: 'Topic', value: ticket.topic},
-                {name: 'Important note', value: ticket.importantNote},
-                {name: 'Creator', value: ticket.creatorUsername},
-                {name: 'Department', value: ticket.department}
-            );
+        if (!Types.ObjectId.isValid(ticketIdString)) return;
         
-            const claimButton = new ButtonBuilder()
-                .setCustomId('claimTicket')
-                .setLabel('Claim')
-                .setStyle(ButtonStyle.Success)
-                .setDisabled(false);
-    
-            const row = new ActionRowBuilder()
-                .addComponents(claimButton);
-    
-            ticketMessage.edit({
-                content: `<@&${pings[departmentSplit[1]]}>`,
-                embeds: [ticketEmbed],
-                components: [row]
+        const ticketId = new Types.ObjectId(ticketIdString);
+        const ticket = await tickets.findOne({ creatorId: message.author.id, _id: ticketId }).exec();
+        if (!ticket) return;
+
+        const content = message.content.replace(/\[.*?\]/, '').trim();
+        if (content.length > 500) {
+            sendDM('This response exceeds the 500 character limit. Please shorten your response or send multiple messages.');
+            return;
+        }
+
+        if (ticket.claimedId === '0') {
+            sendDM("This ticket isn't claimed currently. Please wait until it has been claimed before responding.");
+            return;
+        }
+
+        if (ticket.claimedId === '-1') {
+            sendDM("This ticket has been closed. You can't respond to a closed ticket.");
+            return;
+        };
+
+        let logEntry = `<@${message.author.id}>: ${content}`;
+        if (message.attachments.size > 0) {
+            const attachmentLinks = message.attachments.map(attachment => attachment.url).join('\n');
+            logEntry += `\n**Attachments:**\n${attachmentLinks}`;
+        }
+        ticket.log.push(logEntry);
+        await ticket.save();
+
+        const claimedUser = await client.users.fetch(ticket.claimedId);
+        if (!claimedUser) {
+            message.reply({ content: 'Could not find the user.' }).catch(console.warn);
+            return;
+        }
+
+        const replyMessage = `Reply from <@${message.author.id}> for ticket ID \`${String(ticket._id)}\`:\n\`\`\`${content}\`\`\``;
+        claimedUser.send({ content: replyMessage, files: message.attachments.map(attachment => attachment.url) })
+            .catch(e => {
+                console.warn(e);
+                message.reply({
+                    content: "Something went wrong when replying to the ticket. It will be set to unclaimed to avoid further issues."
+                }).catch(console.warn);
+                ticket.claimedId = '0';
+                ticket.save();
+
+                const departmentSplit = ticket.department.split('-');
+                const category = ticketChannels[departmentSplit[0]];
+                const channel = client.channels.cache.get(category.channel);
+                channel.messages.fetch(ticket.ticketMessageId).then(ticketMessage => {
+                    const ticketEmbed = new EmbedBuilder()
+                        .setTitle('🎫 Ticket')
+                        .setDescription('Awaiting to be claimed')
+                        .addFields(
+                            { name: 'ID', value: String(ticket._id) },
+                            { name: 'Topic', value: ticket.topic },
+                            { name: 'Important note', value: ticket.importantNote },
+                            { name: 'Creator', value: ticket.creatorUsername },
+                            { name: 'Department', value: ticket.department }
+                        );
+
+                    const claimButton = new ButtonBuilder()
+                        .setCustomId('claimTicket')
+                        .setLabel('Claim')
+                        .setStyle(ButtonStyle.Success)
+                        .setDisabled(false);
+
+                    const row = new ActionRowBuilder().addComponents(claimButton);
+                    ticketMessage.edit({ content: `<@&${category.pings[departmentSplit[1]]}>`, embeds: [ticketEmbed], components: [row] })
+                        .catch(console.warn);
+                }).catch(console.warn);
             });
 
-            return;
-        });
-
-        message.reply({
-            content: 'Your response has been sent.'
-        }).catch(e2 => {
-            console.warn(e2);
-        });
+        message.reply({ content: 'Your response has been sent.' }).catch(console.warn);
     } catch (error) {
-        message.reply({
-            content: 'An unexpected error occurred.',
-            ephemeral: true
-        }).catch( e3 => {
-            console.warn(e3);
-        });
         console.warn(error);
-    };
+        message.reply({ content: 'An unexpected error occurred.' }).catch(console.warn);
+    }
 };
