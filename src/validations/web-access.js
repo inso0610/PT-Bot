af39e825e3a23e4da7af8ad6059435126e685100
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-12 19:04:24 +0100

    Add web access validation for command interactions

---
+++ src/validations/web-access.js
@@ -0,0 +1,18 @@
+module.exports = ({ interaction, commandObj }) => {
+    if (commandObj.webAccess) {
+        if (interaction.guild === null) {
+            interaction.reply({
+                content: 'This command can not be ran in DM\'s.',
+                ephemeral: true
+            });
+            return true;
+        } else if (!interaction.member.roles.cache.has('1281956551178981566') && !interaction.member.roles.cache.has('1304849124528754729') && !interaction.member.roles.cache.has('1140260309915938866')) {
+            interaction.reply({
+                content: 'You do not have access to this command.',
+                ephemeral: true
+            });
+
+            return true;
+        };
+    };
+};
