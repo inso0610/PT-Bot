f16e1126d0e3b17ed883a372afc159ba9ec60c12
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-15 21:15:42 +0100

    Fix formatting of replies in ping command to include space before mentions

--- src/events/messageCreate/ping.js
+++ src/events/messageCreate/ping.js
@@ -12,11 +12,11 @@ module.exports = (message) => {
 
     if (content.includes('[PING]') && message.member.roles.cache.has('1111370796439453777')) {
         if (channelId === '1246904420495523925' || channelId === '1337095950027456603') { //trainings
-            message.reply('^<@&1140220447535923200>');
+            message.reply('^ <@&1140220447535923200>');
         } else if (channelId === '1149803344852959273'){ //event-announcements
-            message.reply('^<@&1149802530507862056>');
+            message.reply('^ <@&1149802530507862056>');
         } else if (channelId === '1140363643347816458'){ //shift-announcements
-            message.reply('^<@&1140248514568405044>');
+            message.reply('^ <@&1140248514568405044>');
         };
     };
 };
