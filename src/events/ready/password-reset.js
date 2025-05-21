f9787c171d2cb62fb17b214d0d8d3ceb1bcfda04
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-05-21 17:32:40 +0200

    Refactor: Uncomment password reset stream handling and enhance user notification logic

--- src/events/ready/password-reset.js
+++ src/events/ready/password-reset.js
@@ -1,8 +1,9 @@
 const staffs = require('../../utils/website/staffs');
 const passwordResets = require('../../utils/website/passwordReset');
+const { get } = require('mongoose');
 
 module.exports = async (client) => {
-    try {
+    /*try {
         const changePasswordStream = passwordResets.watch();
 
         changePasswordStream.on('change', async (change) => {
@@ -20,5 +21,30 @@ module.exports = async (client) => {
         });
     } catch (error) {
         console.log(`Error initializing database: ${error}`);
-    }
+    }*/
+
+    async function getResets() {
+        const allResets = await passwordResets.find({}).exec()
+
+        for (const reset of allResets) {
+            const staffDocument = await staffs.findOne({ email: reset.email });
+            if (!staffDocument) return;
+
+            const user = await client.users.fetch(staffDocument.discordId).catch(e => {
+                console.warn(`Error fetching user ${staffDocument.discordId}: ${e}`);
+            });
+
+            if (user) {
+                user.send(`You have requested a password reset. Please use the following link to reset your password: https://www.polartracks.no/PasswordReset/${reset.token}`).catch(e => {
+                    console.warn(`Error sending password reset link to ${user.username}: ${e}`);
+                });
+            } else {
+                console.warn(`User with ID ${staffDocument.discordId} not found.`);
+            }
+        };
+    };
+
+    getResets();
+
+    setInterval(getResets, 60_00); 
 };
