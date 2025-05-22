ec302fa3e56f8b3ec7478d6be6b2afd2178f31d7
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2025-05-22 23:14:21 +0200

    Refactor info-screen.js

--- src/commands/fun/info-screen.js
+++ src/commands/fun/info-screen.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 const wait = require('node:timers/promises').setTimeout;
 
 const images = './src/utils/images';
-//const stationCodes = require('../../utils/trainings.js');
-
 const commandTimeout = require('../../utils/commandTimeout');
 
 module.exports = {
@@ -13,38 +11,32 @@ module.exports = {
         .setName('info-screen')
         .setDescription('Get a station information screen or Real Time Display.')
         .setContexts(['Guild'])
-        .addStringOption((option) =>
-            option
-                .setName('station')
+        .addStringOption(option =>
+            option.setName('station')
                 .setDescription('What station do you want to look at? This is based on station codes.')
                 .setRequired(true))
-        .addStringOption((option) =>
-            option
-                .setName('track')
+        .addStringOption(option =>
+            option.setName('track')
                 .setDescription('Do you want to see the next departure for a specific track? Type in the track number.')
-                .setRequired(false)
-        )
-        .addStringOption((option) =>
-            option
-                .setName('content')
+                .setRequired(false))
+        .addStringOption(option =>
+            option.setName('content')
                 .setDescription('Do you want to see arrivals or departures? (Does not do anything with track is specified)')
                 .setRequired(false)
                 .addChoices(
                     { name: 'Departures', value: 'departure' },
                     { name: 'Arrivals', value: 'arrival' }
                 ))
-        .addStringOption((option) =>
-            option
-                .setName('layout')
+        .addStringOption(option =>
+            option.setName('layout')
                 .setDescription('Should the screen be in landscape or portrait mode? (Does not do anything with track is specified)')
                 .setRequired(false)
                 .addChoices(
                     { name: 'Landscape', value: 'landscape' },
                     { name: 'Portrait', value: 'portrait' }
                 ))
-        .addStringOption((option) =>
-            option
-                .setName('notice')
+        .addStringOption(option =>
+            option.setName('notice')
                 .setDescription('Should station notices show up? (Does not do anything with track specified)')
                 .setRequired(false)
                 .addChoices(
@@ -52,7 +44,7 @@ module.exports = {
                     { name: 'No', value: 'no' }
                 )),
 
-    run: async ({ interaction, client, handler }) => {
+    run: async ({ interaction }) => {
         await interaction.deferReply();
 
         const stationCode = interaction.options.getString('station').toUpperCase();
@@ -61,94 +53,87 @@ module.exports = {
         const layout = interaction.options.getString('layout') ?? 'landscape';
         const notice = interaction.options.getString('notice') ?? 'yes';
 
-        let link;
+        let link = track === null
+            ? `https://rtd.banenor.no/web_client/std?station=${stationCode}&layout=${layout}&content=${content}&notice=${notice}`
+            : `https://rtd.kv.banenor.no/web_client/std?station=${stationCode}&header=no&content=track&track=${track}`;
 
-        if (track === null) {
-            link = `https://rtd.banenor.no/web_client/std?station=${stationCode}&layout=${layout}&content=${content}&notice=${notice}`
-        } else {
-            link = `https://rtd.kv.banenor.no/web_client/std?station=${stationCode}&header=no&content=track&track=${track}`
-        };
-
-        // check if user has role
+        // Check for cooldown unless user has role
         if (!interaction.member.roles.cache.has('1140760173128982588')) {
-            // Check if the command is in the cooldown period
             const commandName = interaction.commandName;
             const userId = interaction.user.id;
+
             const timeout = await commandTimeout.findOne({
                 discordId: userId,
                 command: commandName
             });
 
             if (timeout) {
-
-
                 await interaction.editReply({
                     content: `${link}\n\n\You have to wait until <t:${Math.floor(timeout.expiration.getTime() / 1000)}:t> if you want to get an image from this command. **Server boosters can use this command without cooldown.**`
                 });
                 return;
             }
 
-            // Set a new timeout for the command (3 hours)
-            const expiration = Date.now() + 3 * 60 * 60 * 1000; // 3 hours in milliseconds
+            const expiration = Date.now() + 3 * 60 * 60 * 1000;
             const newTimeout = await commandTimeout.create({
                 discordId: userId,
                 command: commandName,
                 expiration: expiration
             });
 
-            await newTimeout.save()
+            await newTimeout.save();
         }
 
+        let browser;
+
         try {
-            try {
-                const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], ignoreDefaultArgs: ['--disable-extensions'] });
-                const page = await browser.newPage()
+            browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                ignoreDefaultArgs: ['--disable-extensions']
+            });
 
-                if (layout === 'landscape' || track !== null) {
-                    await page.setViewport({
-                        width: 1920,
-                        height: 1080,
-                        deviceScaleFactor: 1
-                    });
-                } else if (layout === 'portrait') {
-                    await page.setViewport({
-                        width: 1080,
-                        height: 1920,
-                        deviceScaleFactor: 1
-                    });
-                };
+            const page = await browser.newPage();
 
-                page.emulateTimezone('Europe/Oslo');
-
-                await page.goto(link);
-
-                await wait(4_000);
-
-                const imagePath = `${images}/screen-${interaction.id}.png`;
-
-                await page.screenshot({ path: imagePath });
-
-                if (fs.existsSync(imagePath)) {
-                    console.log("Screenshot saved");
-
-                    await interaction.editReply({
-                        content: link,
-                        files: [{ attachment: imagePath }]
-                    });
-
-                    fs.unlinkSync(imagePath);
-                } else {
-                    console.log("Screenshot not saved");
-                    interaction.editReply(link);
-                };
-
-                await browser.close();
-            } catch (error) {
-                console.warn(error);
-                interaction.editReply(link);
+            if (layout === 'landscape' || track !== null) {
+                await page.setViewport({
+                    width: 1920,
+                    height: 1080,
+                    deviceScaleFactor: 1
+                });
+            } else if (layout === 'portrait') {
+                await page.setViewport({
+                    width: 1080,
+                    height: 1920,
+                    deviceScaleFactor: 1
+                });
             }
+
+            page.emulateTimezone('Europe/Oslo');
+            await page.goto(link);
+            await wait(4000);
+
+            const imagePath = `${images}/screen-${interaction.id}.png`;
+            await page.screenshot({ path: imagePath });
+
+            if (fs.existsSync(imagePath)) {
+                console.log("Screenshot saved");
+
+                await interaction.editReply({
+                    content: link,
+                    files: [{ attachment: imagePath }]
+                });
+
+                fs.unlinkSync(imagePath);
+            } else {
+                console.log("Screenshot not saved");
+                await interaction.editReply({ content: link });
+            }
+
         } catch (error) {
-            console.warn(error)
+            console.warn(error);
+            await interaction.editReply({ content: link });
+        } finally {
+            if (browser) await browser.close();
         }
     },
 
@@ -159,4 +144,4 @@ module.exports = {
         botPermissions: ['Administrator'],
         deleted: false,
     },
-}
+};
