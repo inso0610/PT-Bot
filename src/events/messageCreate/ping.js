2a0101f9d749538818af80476d9b8d4506b7352e
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-05-25 19:31:12 +0200

    Fix: Update role access validation to include additional role check for '[PING]' command

--- src/events/messageCreate/ping.js
+++ src/events/messageCreate/ping.js
@@ -10,7 +10,7 @@ module.exports = (message) => {
     const channelId = message.channelId;
     const content = message.content;
 
-    if (content.toUpperCase().includes('[PING]') && message.member.roles.cache.has('1111370796439453777')) {
+    if (content.toUpperCase().includes('[PING]') && (message.member.roles.cache.has('1111370796439453777') || message.member.roles.cache.has('1140260309915938866'))) {
         if (channelId === '1246904420495523925' || channelId === '1337095950027456603') { //trainings
             message.reply('^ <@&1140220447535923200>');
         } else if (channelId === '1149803344852959273'){ //event-announcements
