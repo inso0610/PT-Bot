a85b9c63890ac8c3a278b4208b3147aa70896298
Emilsen <emil.v.soldal@gmail.com>
2025-05-27 11:51:21 +0200

    Fix: Enhance train fetching logic and update message handling for multiple train stations

--- src/events/ready/skogvikenAd.js
+++ src/events/ready/skogvikenAd.js
@@ -53,16 +53,16 @@ module.exports = async (client) => {
             return;
         }
 
-        const nextTrains = SKTrains.filter(train => {
+        const nextTrainsSK = SKTrains.filter(train => {
             const trainTime = new Date(train.departure);
             const now = new Date();
             return trainTime > now && train.hasPassed === false && train.isCancelledAtStation === false && train.stopType === 'passenger' && train.fullRoute[train.fullRoute.length - 1].code !== 'SK';
         });
 
-        const nextTrain = nextTrains[0];
+        const nextTrainSK = nextTrainsSK[0];
 
-        let nextTrainMessage = 'No trains are scheduled for today.';
-        if (nextTrain) {
+        let nextTrainMessageSK = 'No trains are scheduled for today.';
+        if (nextTrainSK) {
             const departureTime = new Date(nextTrain.departure);
             const defaultDepartureTime = new Date(nextTrain.defaultDeparture);
 
@@ -73,7 +73,80 @@ module.exports = async (client) => {
             if (departureTime.getTime() !== defaultDepartureTime.getTime())
                 departureTimeString = `~~<t:${Math.floor(defaultDepartureTime.getTime() / 1000)}:t>~~ <t:${Math.floor(departureTime.getTime() / 1000)}:t>`;
 
-            nextTrainMessage = `*${routeString} train to ${nextTrain.fullRoute[nextTrain.fullRoute.length - 1].name} departs at ${departureTimeString}.*\n*Train Number:* ${nextTrain.trainNumber}\n*Operator:* ${operatorString}`;
+            nextTrainMessageSK = `*${routeString} train to ${nextTrain.fullRoute[nextTrain.fullRoute.length - 1].name} departs at ${departureTimeString}.*\n*Train Number:* ${nextTrain.trainNumber}\n*Operator:* ${operatorString}`;
+        };
+
+        const KLHTrains = await fetch(`${process.env.TIOS_API_URL}/locations/KLH/departures`).then(res => res.json()).catch(e => {
+            console.warn(`Error fetching KLHTrains: ${e}`);
+        });
+
+        if (!KLHTrains) {
+            console.warn(`KLHTrains is undefined. Fetch might have failed. Tried to fetch: ${process.env.TIOS_API_URL}/locations/KLH/departures`);
+            return;
+        }
+
+        if (KLHTrains.error !== undefined) {
+            console.warn(`KLHTrains error: ${KLHTrains.error}`);
+            return;
+        }
+
+        const nextTrainsKLH = KLHTrains.filter(train => {
+            const trainTime = new Date(train.departure);
+            const now = new Date();
+            return trainTime > now && train.hasPassed === false && train.isCancelledAtStation === false && train.stopType === 'passenger' && train.fullRoute[train.fullRoute.length - 1].code !== 'KLH';
+        });
+
+        const nextTrainKLH = nextTrainsKLH[0];
+        let nextTrainMessageKLH = 'No trains are scheduled for today.';
+        if (nextTrainKLH) {
+            const departureTime = new Date(nextTrainKLH.departure);
+            const defaultDepartureTime = new Date(nextTrainKLH.defaultDeparture);
+
+            const operatorString = operators[nextTrainKLH.operator] || nextTrainKLH.operator;
+            const routeString = nextTrainKLH.routeNumber || operatorString;
+
+            let departureTimeString = `<t:${Math.floor(departureTime.getTime() / 1000)}:t>`;
+            if (departureTime.getTime() !== defaultDepartureTime.getTime())
+                departureTimeString = `~~<t:${Math.floor(defaultDepartureTime.getTime() / 1000)}:t>~~ <t:${Math.floor(departureTime.getTime() / 1000)}:t>`;
+
+            nextTrainMessageKLH = `*${routeString} train to ${nextTrainKLH.fullRoute[nextTrainKLH.fullRoute.length - 1].name} departs at ${departureTimeString}.*\n*Train Number:* ${nextTrainKLH.trainNumber}\n*Operator:* ${operatorString}`;
+        };
+
+        const RUSTrains = await fetch(`${process.env.TIOS_API_URL}/locations/RUS/departures`).then(res => res.json()).catch(e => {
+            console.warn(`Error fetching RUSTrains: ${e}`);
+        });
+
+        if (!RUSTrains) {
+            console.warn(`RUSTrains is undefined. Fetch might have failed. Tried to fetch: ${process.env.TIOS_API_URL}/locations/RUS/departures`);
+            return;
+        }
+
+        if (RUSTrains.error !== undefined) {
+            console.warn(`RUSTrains error: ${RUSTrains.error}`);
+            return;
+        }
+
+        const nextTrainsRUS = RUSTrains.filter(train => {
+            const trainTime = new Date(train.departure);
+            const now = new Date();
+            return trainTime > now && train.hasPassed === false && train.isCancelledAtStation === false && train.stopType === 'passenger' && train.fullRoute[train.fullRoute.length - 1].code !== 'RUS';
+        });
+
+        const nextTrainRUS = nextTrainsRUS[0];
+
+        let nextTrainMessageRUS = 'No trains are scheduled for today.';
+        if (nextTrainRUS) {
+            const departureTime = new Date(nextTrainRUS.departure);
+            const defaultDepartureTime = new Date(nextTrainRUS.defaultDeparture);
+
+            const operatorString = operators[nextTrainRUS.operator] || nextTrainRUS.operator;
+            const routeString = nextTrainRUS.routeNumber || operatorString;
+
+            let departureTimeString = `<t:${Math.floor(departureTime.getTime() / 1000)}:t>`;
+            if (departureTime.getTime() !== defaultDepartureTime.getTime())
+                departureTimeString = `~~<t:${Math.floor(defaultDepartureTime.getTime() / 1000)}:t>~~ <t:${Math.floor(departureTime.getTime() / 1000)}:t>`;
+
+            nextTrainMessageRUS = `*${routeString} train to ${nextTrainRUS.fullRoute[nextTrainRUS.fullRoute.length - 1].name} departs at ${departureTimeString}.*\n*Train Number:* ${nextTrainRUS.trainNumber}\n*Operator:* ${operatorString}`;
         };
 
         const embed = new EmbedBuilder()
@@ -81,7 +154,9 @@ module.exports = async (client) => {
             .setTitle('Play Skogviken Kommune!')
             .setDescription('Create your story in a dynamic Arctic town with endless possibilities. Shape the world around you and be part of an evolving community.')
             .addFields(
-                { name: 'Next train from Skogviken Station', value: nextTrainMessage, inline: false },
+                { name: 'Next train from Skogviken Station', value: nextTrainMessageSK, inline: false },
+                { name: 'Next train from Kirkenes Lufthavn Høybuktmoen Station', value: nextTrainMessageKLH, inline: false },
+                { name: 'Next train from Rustfjelbma Station', value: nextTrainMessageRUS, inline: false }
             )
             .setTimestamp()
             .setFooter({ text: 'Skogviken Kommune' });
