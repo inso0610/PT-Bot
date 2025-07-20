96245db384602167fafcf05667232286406614f5
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-07-20 20:11:17 +0200

    fix: Update console log in getStationData function to log mainResponse instead of mainJson

--- src/utils/entur.js
+++ src/utils/entur.js
@@ -34,7 +34,7 @@ const getStationData = async function (id, getChild = true, childToGet = 'RAIL_S
 
     const mainJson = await mainResponse.json();
 
-    console.log(mainJson);
+    console.log(mainResponse);
 
     if (!mainJson.root || Object.keys(mainJson.root).length === 0) {
         console.error('No root data found in the response');
